11e32a1dd3eb0d2b1e1dbd18e282677d349dce1c
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-24 13:13:27 -0500

    Add key-value protocol definitions

--- protocols.py
+++ protocols.py
@@ -5,24 +5,32 @@
 # ======================
 # Data structure:
 # ======================
+# (Key is tied to the protocol id)
 # name: Name of datatype
 # id:   Protocol ID number
 # data: List of all data sections of the packet
 #     name: Name of data
 #     type: Datatype
-PROTOCOLS = [
-  {
-    "name" : "location",
-    "id"   : 1,
-    "data" : [
-      {
-        "name" : "latitude",
-        "type" : float
-      },
-      {
-        "name" : "longitude",
-        "type" : float
-      }
-    ]
-  }
-]
+PROTOCOLS = {}
+
+# Location Protocol
+PROTOCOLS[1] = {
+                "name" : "location",
+                "id"   : 1,
+                "data" : [
+                  {
+                    "name" : "latitude",
+                    "type" : float,
+                  },
+                  {
+                    "name" : "longitude",
+                    "type" : float
+                  }
+                ]
+              }
+
+# TODO: Needs an endpoint field for the API
+
+# Helper fn
+def getTypeStr(t):
+  if (t == float): return "f"
