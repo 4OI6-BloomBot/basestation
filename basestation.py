6014a2c23dd91260392e16f87af746373697b169
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2024-01-13 19:17:14 -0500

    Add middleware to basestation.py. Fix imports

--- basestation.py
+++ basestation.py
@@ -4,8 +4,9 @@
 
 # Imports
 import threading
-from   radio         import Radio
-from   packet_parser import PacketParser
+from   src.radio             import Radio
+from   src.packet_parser     import PacketParser
+from   src.server_middleware import ServerMiddleware
 
 # ==================================================
 # Global vars:
@@ -22,16 +23,18 @@ data_queue   = []
 # Create a radio object and start a loop to listen for data
 radio  = Radio(packet_queue)
 parser = PacketParser(packet_queue, data_queue)
+server = ServerMiddleware(data_queue)
 
 
 # TODO: Should be in a separate thread and push results to a queue
 #       A second thread can then pull from the queue to send data
 #       to the server.
 rxThread     = threading.Thread(target=radio.listen)
-parserThread = threading.Thread(target=parser.monitorRxQueue)
 txThread     = threading.Thread(target=radio.send)
+parserThread = threading.Thread(target=parser.monitorRxQueue)
+serverThread = threading.Thread(target=server.monitorServerQueue)
 
 rxThread.start()
 parserThread.start()
-txThread.start()
-
+serverThread.start()
+# txThread.start()
