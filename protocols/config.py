1ba9892be2ae6ab5b973121243c7c6ade0591633
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2024-02-08 20:34:05 -0500

    Add Config protocol

---
+++ protocols/config.py
@@ -0,0 +1,71 @@
+# =============================================
+# Location protocol class
+# =============================================
+
+# ============================
+# Imports
+# ============================
+from .base import BaseProtocol, PROTOCOLS
+
+
+class Config(BaseProtocol):
+
+  # =============================================
+  # Declare the data structure for the protocol
+  # =============================================
+  data = {
+    "id" : {
+      "type": int
+    },
+    "temp_thresh" : {
+      "type"      : float,
+      "precision" : 6
+    }
+  }
+
+  endpoint = "config"
+  name     = "Config"
+
+
+  # =============================================
+  # Constructor
+  # ============================================= 
+  def __init__(self):
+    super().__init__(1)
+
+
+  # =============================================
+  # Won't be used for the Config protocol
+  # =============================================
+  def packJSONData(self, json):
+    pass
+
+
+  def parseJSON(self, json):
+    for key in json:
+      if (key in self.data):
+        self.setValue(json[key], key)
+
+      # TODO: Should log params that are not in the param
+      # raise ValueError("Parameter {p} does not exist in the Config protocol.".format(p = key))
+        
+
+  # ==================================================
+  # Returns a list containing all values of the packet
+  # ==================================================
+  def getValuesList(self):
+    values = []
+
+    for key in self.data:
+      if ("value" not in self.data[key]):
+        raise ValueError("Value not provided for {name}".format(name = key))
+      else:
+        values.append(self.data[key]["value"])
+    
+    return values
+
+
+# =============================================
+# Add Config to protocol type dictionary
+# =============================================
+PROTOCOLS[1] = Config
